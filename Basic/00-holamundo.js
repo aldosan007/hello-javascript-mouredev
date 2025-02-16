66877465afc956dfc16d066bdff5d4c3e13a7c48
aldosan007 <aldorene5411@gmail.com>
2025-02-16 20:51:00 -0300

    Modificado 00-holamundo.js

--- Basic/00-holamundo.js
+++ Basic/00-holamundo.js
@@ -1,1 +1,24 @@
+//Esto  es un comentario simple
+
+/*
+Hola 
+este es un 
+comentario 
+de varias
+lineas
+*/
 console.log("Hola, mundo");
+console.log('Hola, mundo');
+console.log(`Hola, mundo`);
+
+// console.log("Hola, comentario");
+
+
+console.log(23);
+console.log("23");
+console.log(23 + 2);
+console.log(23 - 2);
+console.log(23 * 2);
+console.log(23 / 2);
+console.log(23 % 2);
+console.log(23 ** 2);
